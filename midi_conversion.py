07f4d1988eedf8053fbaa813889f387f5b94628f
Zidane Karim <zkarim7676@gmail.com>
2023-03-12 12:59:56 -0400

    working midi conversion

--- midi_conversion.py
+++ midi_conversion.py
@@ -27,17 +27,19 @@ def notes_to_freq(notes):
     freqs = []
     for note in notes:
         print(note)
-        note_name = note[:-2]
-        print(1)
+        note_name = note[0]
+        if note[1] == "♭" or note[1] == "♯":
+            note_name = note[:2]
+        #print(1)
         octave = int(note[-1])
         if len(note) == 4: # check for 2-digit octave
             octave = int(note[-2:])
-        print(2)
-        print(note_name)
+        #print(2)
+        #print(note_name)
         note_index = NOTE_NAMES.index(note_name)
-        print(3)
+        #print(3)
         freq = BASE_FREQ * (2 ** ((note_index - 9) / 12)) * (2 ** octave)
-        print(4)
+        #print(4)
         freqs.append(freq)
         print(freq)
     return freqs
